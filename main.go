16e4af991a6d83d23b55138575d474ed88147cf9
pedromessetti <pedromessetti@gmail.com>
2023-09-02 14:21:57 +0100

    add user routes

---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	routes "auth_project/routes"
+	"os"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8000"
+	}
+
+	router := gin.New()
+	router.Use(gin.Logger())
+
+	routes.AuthRoutes(router)
+	routes.UserRoutes(router)
+
+	router.GET("/api-1", func(c *gin.Context) {
+		c.JSON(200, gin.H{"sucess": "Access granted for api-1"})
+	})
+
+	router.GET("/api-2", func(c *gin.Context) {
+		c.JSON(200, gin.H{"sucess": "Access granted for api-2"})
+	})
+
+	router.Run(":" + port)
+}
