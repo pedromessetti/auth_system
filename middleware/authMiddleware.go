e5a04abe489ac74f5bc42d20459cc0eece793049
pedromessetti <pedromessetti@gmail.com>
2023-09-04 11:47:56 +0100

    improve redability of error messages

--- middleware/authMiddleware.go
+++ middleware/authMiddleware.go
@@ -12,14 +12,14 @@ func Authenticate() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		clientToken := c.Request.Header.Get("token")
 		if clientToken == "" {
-			c.JSON(http.StatusInternalServerError, gin.H{"error": "Unauthorized"})
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Unauthorized [missing token]"})
 			c.Abort()
 			return
 		}
 
 		claims, err := helper.ValidateToken(clientToken)
 		if err != "" {
-			c.JSON(http.StatusInternalServerError, gin.H{"error": "Unauthorized"})
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Unauthorized [invalid token]"})
 			c.Abort()
 			return
 		}
